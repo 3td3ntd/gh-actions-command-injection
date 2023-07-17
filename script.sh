d48b4ce4e97cc0733cf13b7535efcc90f87a7d17
Kanumuru Rajesh <rajesh@we45.com>
2023-07-17 17:36:06 +0530

    added tf fikes and update the code for workflow.yml

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo su
+sudo apt-get update -y
+export AWS_ACCESS_KEY=AKIAJSIE27KKMHXI3BJQ
+export AWS_SECRET_KEY=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 
+export API_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzNDU2Nzg5LCJuYW1lIjoiSm9zZXBoIn0.OpOSSw7e485LOP5PrzScxHb7SR6sAOMRckfFwi4rp7o
+
